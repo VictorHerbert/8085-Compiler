bdf007c3aee68ad24f505149ed8e485a59c24bad
Victor Herbert <victorherberts@outlook.com>
2022-10-10 01:01:42 -0300

    Added instructions encoding

--- compiler/__main__.py
+++ compiler/__main__.py
@@ -16,17 +16,6 @@ args = parser.parse_args()
 code = args.i.read()
 compiler = Compiler(code, args.i.name)
 
-
-isa = {
-    'org'   : Instruction(fr'{EXPRESSION}', 0, lambda args: []),
-    'jmp'   : Instruction(fr'{EXPRESSION}', 3, lambda args: [20, compiler.evaluate_expr(args[0])>>compiler.WORD_SIZE, compiler.evaluate_expr(args[0])%compiler.WORD_SIZE]),
-    'db'    : Instruction(fr'{EXPRESSION}', 1, lambda args: [compiler.evaluate_expr(args[0])]),
-    'ds'    : Instruction(fr'{EXPRESSION}', lambda args: int(args[0]), lambda args: [0]*int(args[0])),
-    'mvi'   : Instruction(fr'{REGISTER}, *{EXPRESSION}', 2, lambda args: [66,67])
-}
-
-compiler.set_isa(isa)
-
 error_count = compiler.compile(args.o, args.l)
 
 if error_count == 0:
--- compiler/compiler.py
+++ compiler/compiler.py
@@ -1,4 +1,5 @@
 from fileinput import filename
+from msilib.schema import File
 import re
 from typing import Dict
 
@@ -28,17 +29,122 @@ class Compiler:
         self.output_file = output_file
         self.listings_file = listings_file
 
-    def set_isa(self, isa: Dict[str, Instruction]) -> None:
-        self.isa = isa
+        macros = {
+            'org'   : Instruction(fr'{EXPRESSION}', 0, lambda args: []),
+            'db'    : Instruction(fr'{EXPRESSION}', 1, lambda args: [self.evaluate_expr(args[0])]),
+            'ds'    : Instruction(fr'{EXPRESSION}', lambda args: int(args[0]), lambda args: [0]*int(args[0]))
+        }
 
+        single_inst = lambda op : Instruction([], 1, lambda args: [op])
+        imm_inst = lambda op : Instruction([fr'{EXPRESSION}'], 2, lambda args: [op, self.evaluate_expr(args[0])])
+        addr_imm_inst = lambda op : Instruction([fr'{EXPRESSION}'], 3, lambda args: [op, self.evaluate_expr(args[0])%256, self.evaluate_expr(args[0])//256])
+        reg_inst_s = lambda op : Instruction([fr'{REGISTER}'], 1, lambda args: [op + self.evaluate_register(args[0])])
+        reg_inst_d = lambda op : Instruction([fr'{REGISTER}'], 1, lambda args: [op + self.evaluate_register(args[0])<<3])
+        reg_inst_ds = lambda op : Instruction([fr'{REGISTER}', fr'{REGISTER}'], 1, lambda args: [op + self.evaluate_register(args[0])<<3 + self.evaluate_register(args[1])])
+        reg_imm_inst_d = lambda op : Instruction([fr'{REGISTER}', fr'{EXPRESSION}'], 2, lambda args: [op + self.evaluate_register(args[0])<<3, self.evaluate_expr(args[1])])
+        reg_double_inst = lambda op : Instruction([fr'{REGISTER}'], 1, lambda args: [op + self.evaluate_double_register(args[0])<<4])
+        reg_double_inst_r = lambda op : Instruction([fr'{REGISTER}'], 1, lambda args: [op + self.evaluate_bd_register(args[0])<<4])
+        reg_double_inst_addr = lambda op : Instruction([fr'{REGISTER}', fr'{EXPRESSION}'], 3, lambda args: [op + self.evaluate_double_register(args[0])<<4, self.evaluate_expr(args[1])%256, self.evaluate_expr(args[1])//256])
+
+        #TODO evaluete with fixed size
+        rst_inst = lambda op : Instruction([fr'{NUMBER}'], 1, lambda args: [op + self.evaluate_expression(args[0])<<3])
+
+        instructions = {
+            'aci':  imm_inst(0b11001110),
+            'adc':  reg_inst_s(0b10001000),
+            'add':  reg_inst_s(0b10000000),
+            'adi':  imm_inst(0b11000110),
+            'ana':  reg_inst_s(0b10100000),
+            'ani':  imm_inst(0b11100110),
+            'call': addr_imm_inst(0b11001101),
+            'cc':   addr_imm_inst(0b11011100),
+            'cm':   addr_imm_inst(0b11111100),
+            'cma':  single_inst(0b00101111),
+            'cmc':  single_inst(0b00111111),
+            'cmp':  reg_inst_s(0b10111000),
+            'cnc':  addr_imm_inst(0b11011100),
+            'cnz':  addr_imm_inst(0b11000100),
+            'cp':   addr_imm_inst(0b11110100),
+            'cpe':  addr_imm_inst(0b11101100),
+            'cpi':  imm_inst(0b11111110),
+            'cpo':  addr_imm_inst(0b11100100),
+            'cz':   addr_imm_inst(0b11001100),
+            'daa':  single_inst(0b00100111),
+            'dad':  reg_double_inst(0b00001001),
+            'dcr':  reg_inst_d(0b00000101),
+            'dcx':  reg_double_inst(0b00001011),
+            'ei':   single_inst(0b11111011),
+            'hlt':  single_inst(0b01110110),
+            'in':   imm_inst(0b11011011),
+            'inr':  reg_inst_d(0b00000100),
+            'inx':  reg_double_inst(0b00000011),
+            'jc':   addr_imm_inst(0b11011010),
+            'jm':   addr_imm_inst(0b11111010),
+            'jmp':  addr_imm_inst(0b11000011),
+            'jnc':  addr_imm_inst(0b11010010),
+            'jnz':  addr_imm_inst(0b11000010),
+            'jp':   addr_imm_inst(0b11110010),
+            'jpe':  addr_imm_inst(0b11101010),
+            'jpo':  addr_imm_inst(0b11100010),
+            'jz':   addr_imm_inst(0b11001010),
+            'lda':  addr_imm_inst(0b00111010),
+            'ldax': reg_double_inst_r(0b00001010),
+            'lhld': addr_imm_inst(0b00101010),
+            'lxi':  reg_double_inst_addr(0b00000001),
+            'mov':  reg_inst_ds(0b01000110),
+            'mvi':  reg_imm_inst_d(0b00000110),
+            'not':  single_inst(0b00000000),
+            'ora':  reg_inst_d(0b10110000),
+            'ori':  imm_inst(0b11110110),
+            'out':  imm_inst(0b11010011),
+            'phcl': single_inst(0b11101001),
+            'pop':  reg_double_inst(0b11000001),
+            'push': reg_double_inst(0b11000101),
+            'ral':  single_inst(0b00010111),
+            'rar':  single_inst(0b00011111),
+            'rc':   single_inst(0b11011000),
+            'ret':  single_inst(0b11001001),
+            'rim':  single_inst(0b00100000),
+            'rlc':  single_inst(0b00000111),
+            'rm':   single_inst(0b11111000),
+            'rnc':  single_inst(0b11010000),
+            'rnz':  single_inst(0b11000000),
+            'rp':   single_inst(0b11110000),
+            'rpe':  single_inst(0b11101000),
+            'rpo':  single_inst(0b11100000),
+            'rrc':  single_inst(0b00001111),
+            'rst':  rst_inst(0B11000111),
+            'sbb':  reg_inst_s(0b100111000),
+            'sbi':  single_inst(0b11011110),
+            'shld': addr_imm_inst(0b00100010),
+            'sim':  single_inst(0b00110000),
+            'sphl': single_inst(0b11111001),
+            'sta':  addr_imm_inst(0b00110010),
+            'stax': reg_double_inst_r(0b00000010),
+            'stc':  single_inst(0b00110111),
+            'sub':  reg_inst_s(0b10010000),
+            'sui':  single_inst(0b11010110),
+            'xchg': single_inst(0b11101011),
+            'xra':  reg_inst_s(0b10101000),
+            'xri':  imm_inst(0b11101110),
+            'xthl': single_inst(0b11100011),
+        }
+
+        self.isa = instructions | macros
+
+    def  evaluate_register(self, register: str) -> int:
+        raise NotImplementedError()
+
+    def  evaluate_double_register(self, register: str) -> int:
+        raise NotImplementedError()
         
-    def evaluate_expr(self, expr):
+    def evaluate_expr(self, expr: str) -> int:
         expr = re.sub(fr'(?P<init>^|[+-/*()])(?P<name>{NAME})', lambda match: match.group('init') + str(self.labels[match.group('name')]), expr)
         expr = re.sub(r'(?P<hex_number>[0-9a-f]+)h', r'0x\g<hex_number>', expr)
 
         return eval(expr)
    
-    def pre_compile(self, line_idx: int, line: str):
+    def pre_compile(self, line_idx: int, line: str) -> None:
         rg = fr'^(?P<equ_label>{NAME}) *equ *(?P<expression>{EXPRESSION}) *$'
         match = re.match(rg, line)
         if match:
@@ -70,12 +176,12 @@ class Compiler:
                 self.idx += self.isa[mnemonic].length(args)
 
 
-    def compile_instruction(self, instruction: str):
+    def compile_instruction(self, instruction: str) -> None:
         line_idx, idx, mnemonic, args = instruction
         for i, v in enumerate(self.isa[mnemonic].assemble(args)):
             self.memory[idx+i] = v
     
-    def compile(self, output, listing = None):
+    def compile(self, output : File, listing = None) -> int:
         error_count = 0
         for i, line in enumerate(self.lines):
             if line != '':
@@ -108,4 +214,4 @@ class Compiler:
         if not error_count:
             output.write(bytearray(self.memory))
 
-        return  error_count
+        return error_count
